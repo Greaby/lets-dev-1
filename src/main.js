25d0613fb836ecd1639e3cbfae6d31d132448853
Greaby <contact@greaby.com>
2017-07-15 18:40:51 +0200

    episode 1

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import 'pixi'
+import 'p2'
+import Phaser from 'phaser'
+import GameState from './states/Game'
+import config from './config'
+
+class Game extends Phaser.Game {
+	constructor() {
+		super(config.width, config.height)
+		this.state.add("Game", GameState, true)
+	}
+}
+
+window.game = new Game()
---
+++ src/states/Game.js
@@ -0,0 +1,69 @@
+import Phaser from 'phaser'
+import config from '../config'
+import {shuffle} from '../utils'
+
+export default class extends Phaser.State {
+
+	init() {
+		this.level = 8
+		this.selectedTiles = []
+	}
+
+	preload() {
+		this.load.spritesheet("tiles", "assets/tiles.png", 8, 8)
+	}
+
+	create() {
+		this.placeTiles()
+	}
+
+	render() {}
+
+	placeTiles() {
+		const size = config.size * config.scale
+		const [cols, rows] = [4,4]
+
+		const leftSpace = (game.width - cols * size - (cols - 1) * config.spacing) / 2 + size / 2
+		const topSpace = (game.height - rows * size - (rows - 1) * config.spacing) / 2 + size / 2
+
+		let frames = config.playerFrames.slice(0, this.level)
+		frames = frames.concat(frames)
+
+		shuffle(frames)
+
+		for (var i = 0; i < cols; i++) {
+			for (var j = 0; j < rows; j++) {
+				let tile = game.add.button(leftSpace + i * (size + config.spacing), topSpace + j * (size + config.spacing), "tiles", this.showTile, this)
+				tile.anchor.setTo(0.5)
+				tile.smoothed = false
+				tile.scale.x = config.scale
+				tile.scale.y = config.scale
+				tile.value = frames[j * cols + i]
+			}
+		}
+	}
+
+	showTile(target) {
+		if(this.selectedTiles.length < 2 && this.selectedTiles.indexOf(target) === -1) {
+			target.frame = target.value
+			this.selectedTiles.push(target)
+
+			if(this.selectedTiles.length === 2) {
+				game.time.events.add(Phaser.Timer.SECOND / 4, this.checkTiles, this)
+			}
+		}
+	}
+
+	checkTiles(target) {
+		if(this.selectedTiles[0].value === this.selectedTiles[1].value) {
+			this.selectedTiles[0].destroy()
+			this.selectedTiles[1].destroy()
+		} else {
+			this.selectedTiles[0].frame = config.hiddenFrame;
+			this.selectedTiles[1].frame = config.hiddenFrame;
+		}
+		this.selectedTiles = []
+	}
+
+
+}
