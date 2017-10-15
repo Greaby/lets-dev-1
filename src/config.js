627ae050d4406dea6f6f787a6a3cae8318228b1e
Greaby <contact@greaby.com>
2017-10-15 15:39:51 +0200

    add new characters

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    time: 45,
+    time: 90,
     width: 1200,
     height: 1000,
     size: 8,
@@ -7,5 +7,5 @@ export default {
     spacing: 10,
     hiddenFrame: 0,
     mobFrames: [1, 2],
-    playerFrames: [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    playerFrames: [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
 }
