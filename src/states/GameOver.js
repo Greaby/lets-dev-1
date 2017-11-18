946e6c3211f3e0fd5a1701628f69974f293eb40c
Greaby <contact@greaby.com>
2017-11-18 11:51:21 +0100

    add background to game over state

--- src/states/GameOver.js
+++ src/states/GameOver.js
@@ -3,6 +3,14 @@ import Phaser from 'phaser'
 
 export default class extends Phaser.State {
     create () {
+        const w = this.world.width + 50 * 2
+        const h = this.world.height + 50 * 2
+
+        this.background = game.add.tileSprite(-50, -50, w, h, 'background')
+        this.background.tileScale.x = 8
+        this.background.tileScale.y = 8
+        this.background.frame = Math.floor(Math.random() * 16 + 1)
+
         if (this.game.score > this.game.best) {
             this.game.best = this.game.score
         }
@@ -27,4 +35,9 @@ export default class extends Phaser.State {
     }
 
     render () {}
+
+    update () {
+        this.background.tilePosition.x += 0.08
+        this.background.tilePosition.y += 0.09
+    }
 }
